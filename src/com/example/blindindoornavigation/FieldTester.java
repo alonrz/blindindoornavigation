d5c86f5fe2227b0e438f2a89b76865a4b8c32ff5
alon <alon.zilberman@gmail.com>
2013-08-30 16:55:54 -0700

    All buttons work. Start, stop, record step and undo step.

--- src/com/example/blindindoornavigation/FieldTester.java
+++ src/com/example/blindindoornavigation/FieldTester.java
@@ -2,26 +2,75 @@ package com.example.blindindoornavigation;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class FieldTester extends Activity{
+public class FieldTester extends Activity {
 
-	 protected void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.field_test);
-	        
-	 }
-	  
-	 public void onClick_StartWrite(View view) throws IOException{
-		 List<String> items = new ArrayList<String>();
-		 items.add("hello");
-		 items.add("from");
-		 items.add("list");
-		 WriterUtility.writeStep(items);
-	 }
-	        
+	boolean isUndoEnabled = false;
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.field_test);
+	}
+
+	public void onClick_StartWrite(View view) throws IOException {
+		WriterUtility.startTest();
+		Button btnStart = (Button) findViewById(R.id.StartWrite);
+		btnStart.setEnabled(false);
+		Button btnStop = (Button) findViewById(R.id.StopAndSave);
+		btnStop.setEnabled(true);
+
+		Button btnRecordStep = (Button) findViewById(R.id.RecordStep);
+		btnRecordStep.setEnabled(true);
+		Button btnUndo = (Button) findViewById(R.id.Undo);
+		btnUndo.setEnabled(false);
+	}
+
+	public void onClick_StopAndSave(View view) throws IOException {
+		WriterUtility.endTestAndSave();
+		Button btnStart = (Button) findViewById(R.id.StartWrite);
+		btnStart.setEnabled(true);
+		Button btnStop = (Button) findViewById(R.id.StopAndSave);
+		btnStop.setEnabled(false);
+		
+		Button btnRecordStep = (Button) findViewById(R.id.RecordStep);
+		btnRecordStep.setEnabled(false);
+		Button btnUndo = (Button) findViewById(R.id.Undo);
+		btnUndo.setEnabled(false);
+	}
+
+	public void onClick_RecordStep(View view) throws IOException {
+		
+
+		int n = WriterUtility.writeStep(new ArrayList<String>(Arrays.asList(
+				"name of device", "SSID", "some value")));
+		
+		if (isUndoEnabled == false) {
+			Button btnUndo = (Button) findViewById(R.id.Undo);
+			btnUndo.setEnabled(true);
+			isUndoEnabled = true;
+		}
+		
+		//Change number on button
+		Button btnRecordStep = (Button) findViewById(R.id.RecordStep);
+		btnRecordStep.setText("Record Step " + (n+1)); 
+	}
+
+	public void onClick_Undo(View view) {
+		int n = WriterUtility.undoStep();
+		if(n == 0){
+			Button btnUndo = (Button) findViewById(R.id.Undo);
+			btnUndo.setEnabled(false);
+			isUndoEnabled = false;
+		}
+		Button btnRecordStep = (Button) findViewById(R.id.RecordStep);
+		btnRecordStep.setText("Record Step " + (n+1));
+	}
+
 }
--- src/com/example/blindindoornavigation/WriterUtility.java
+++ src/com/example/blindindoornavigation/WriterUtility.java
@@ -11,19 +11,27 @@ import android.text.format.Time;
 
 public class WriterUtility {
 
+	private static StringBuilder sb;
+	private static int n = 0; //n = the number of records (lines) added.
+	
 	//returns number of files in folder
-	public static int writeStep(List<String> itemsToWrite) throws IOException
+	public static void startTest()
 	{
-		int n = 0; //n = the number of records (lines) created in a file
+		sb = new StringBuilder();
+	}
+	
+	public static void endTestAndSave() throws IOException
+	{
 		try{
-			File directory = Environment.getExternalStorageDirectory();
+			File rootDirectory = Environment.getExternalStorageDirectory();
 			Time time = new Time(){{setToNow();}};
 			
-			File myDirectory = new File(directory.getAbsolutePath() + "/indoorNavData");
+			File myDirectory = new File(rootDirectory.getAbsolutePath() + "/indoorNavData");
 			if(myDirectory.exists() == false)
 			{
 				myDirectory.mkdir();
 			}
+			//Create file name with time stamp. 
 			File myFile = new File(myDirectory + "/beacon_data_" + time.format2445() +".csv"); //write new file with time stamp
 			
 			if(myFile.exists() == false)
@@ -33,22 +41,46 @@ public class WriterUtility {
 			FileWriter fw = new FileWriter(myFile.getAbsoluteFile());
 			BufferedWriter writer = new BufferedWriter(fw);  
 			
-			//write content to file
-			writer.write("first value");
-			
-			//Write time
-			writer.write(","+ time.format2445());
-			
-			//
-			
-			
+			writer.write(sb.toString());
 			writer.newLine();
 			writer.close();
 			
-			return n;
+			
 		}catch(IOException e)
 		{
 			throw e;
 		}
+	}
+	public static int writeStep(List<String> itemsToWrite) throws IOException
+	{
+		n++;
+		
+		for(int i=0; i<itemsToWrite.size(); i++)
+		{
+			sb.append(itemsToWrite.get(i));
+			if(i != itemsToWrite.size()-1)
+			{
+				sb.append(",");
+			}
+		}
+		
+		//*** Add more data such as sensors here ***
+		
+		//Add time
+		sb.append("," + new Time(){{setToNow();}}.format2445());
+		
+		
+		sb.append('\n');
+		return n;
+	}
+	
+	public static int undoStep()
+	{
+		int lastNewLine = sb.lastIndexOf("\n");
+		sb.delete(lastNewLine, sb.length());
+		lastNewLine = sb.lastIndexOf("\n");
+		sb.delete(lastNewLine+1, sb.length());
+		n--;
+		return n;
 	}
 }
