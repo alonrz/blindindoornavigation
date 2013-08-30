170a7c50bdf02d2bf2cbe374d600291d40bcdb4c
alon <alon.zilberman@gmail.com>
2013-08-30 13:57:16 -0700

    auto generated files that Git forces me to commit. Not happy about it

--- gen/com/example/blindindoornavigation/R.java
+++ gen/com/example/blindindoornavigation/R.java
@@ -21,11 +21,16 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int border=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int StartWrite=0x7f080002;
-        public static final int action_settings=0x7f080003;
+        public static final int RecordStep=0x7f080005;
+        public static final int StartWrite=0x7f080003;
+        public static final int StopAndSave=0x7f080004;
+        public static final int Undo=0x7f080006;
+        public static final int action_settings=0x7f080007;
+        public static final int linearLayout1=0x7f080002;
         public static final int txtBTIndicator=0x7f080000;
         public static final int txtDeviceOne=0x7f080001;
     }
