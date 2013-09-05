d4def61ac54d242fc0f6041a1e767597371b739a
DavidWebsterHCI <webstercello@gmail.com>
2013-09-04 17:37:08 -0700

    1) Added bluetooth connection strategy 	-page pre-known devices at the ACL level. 	-times out in ~5 seconds if device not in range, or ACL response in ~2-4 seconds if device is in range and in discoverable mode.
    2) spins off X amount of threads based on X amount of MAC addresses hardcoded to do the connection.

--- src/com/example/blindindoornavigation/MainActivity.java
+++ src/com/example/blindindoornavigation/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.blindindoornavigation;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
@@ -16,7 +23,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 
 public class MainActivity extends Activity {
-
+	
 	BluetoothAdapter adapter;
 	int REQUEST_ENABLE_BT = 1;
 	
@@ -42,6 +49,7 @@ public class MainActivity extends Activity {
         {
         	txtBTIndicatr.setText("BT is enabled");
         }
+               
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
@@ -60,57 +68,101 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			String action = intent.getAction();
-			// When discovery finds a device
-			if(BluetoothDevice.ACTION_FOUND.equals(action))
-			{
-				// Get the BluetoothDevice object from the Intent
-				BluetoothDevice device  = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-				// Add the name and address to an array adapter to show in a ListView
-				TextView txtDeviceOne = (TextView)findViewById(R.id.txtDeviceOne);
-				short rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
-				
-				txtDeviceOne.setText(device.getName() + "\n" + device.getAddress() + 
-				"\nRSSI value: " + rssi);
-				
-			}
-		}
-	};
 	
-    public void onClick_Discover(View view)
-    {
-    	if(adapter.startDiscovery()==false)
-    	{
-    		Toast.makeText(this, "Error discovering devices", Toast.LENGTH_SHORT).show();
-    	}
+	public void onClick_Discover(View view)
+    {   	
+    	//Create a BroadcastReceiver for ACL connection success
+    	final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+    	    public void onReceive(Context context, Intent intent) {
+    	        String action = intent.getAction();
+    			
+    	        if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
+    	        	d("ACL success for: " + intent.getExtras().get("android.bluetooth.device.extra.DEVICE").toString());
+    	        }
+    	    }
+    	};
+    	
     	// Register the BroadcastReceiver
-    	IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-    	registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
-    	Toast.makeText(this, "registered to receive device info", Toast.LENGTH_SHORT).show();
+    	IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_ACL_CONNECTED);
+    	registerReceiver(mReceiver, filter);// Don't forget to unregister during onDestroy    	
+    	
+    	//Spin off threads for attempted connection to expected beacons
+    	//TODO make this list not hard coded, but gathered from an external map-based file.
+		ArrayList<String> stringList = new ArrayList<String>();
+		stringList.add("90:00:4E:FE:34:E1");//Laptop
+		stringList.add("78:A3:E4:A8:7E:48");//Phone
+		
+		for(int i=0; i<stringList.size(); i++){
+			AcceptThread at = new AcceptThread(stringList.get(i));
+			at.start();
+		}
     }
     
     @Override
 	protected void onStop() {
 		// TODO Auto-generated method stub
 		super.onStop();
-		
 	}
 
+	@SuppressLint("NewApi")
 	public void onClick_Undiscover(View view)
     {
-    	unregisterReceiver(mReceiver);
+		//TODO need to make sure correct registering unregistering of receiver.
+		//unregisterReceiver(mReceiver);	
     }
 	
 	public void onClick_GoToTestActivity(View view)
 	{
 		Intent myIntent = new Intent(this, FieldTester.class);
 		this.startActivity(myIntent);
-		
-		
+	}
+	
+	//Debug log function
+	public static void d(String s){
+		Log.d("debug", s);
+	}
+	//Debug toast function
+	public void t(String s){
+		Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////ACL Connection Thread Class///////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
+	private class AcceptThread extends Thread {
+	    private String addr;
+	 
+	    public AcceptThread(String macAddress) {
+	        addr = macAddress;
+	    }
+	 
+	    @SuppressLint("NewApi")
+		public void run() {
+	        
+	    	BluetoothSocket a;
+	    	
+	        BluetoothDevice macTest;
+			macTest = adapter.getRemoteDevice(addr);//phone
+			
+	        d("macTest: " + macTest.toString());
+	        UUID t = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");  
+	        
+	        //Attempt to form a "connection" so that the low level ACL data will be sent -- which is all we want
+	        //This try/catch block will always end in an IOexception. 
+	        try{ 
+	        	a = macTest.createInsecureRfcommSocketToServiceRecord(t);
+	        	adapter.cancelDiscovery();
+	        	adapter.listenUsingInsecureRfcommWithServiceRecord("blind", t);
+	        	a.connect();
+	        }catch(Exception e){
+	        	d(addr + " " + e.toString());
+	        }	   
+	    }
 	}
 }
+
+
+
+
+
