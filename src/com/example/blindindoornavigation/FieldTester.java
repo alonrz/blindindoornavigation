5e30fd1c8d7b7bbb6b669c72760ec53a50fac998
alon <alon.zilberman@gmail.com>
2013-09-07 16:42:27 -0700

    added progress bar to wait for beacons to be discovered

--- src/com/example/blindindoornavigation/FieldTester.java
+++ src/com/example/blindindoornavigation/FieldTester.java
@@ -9,21 +9,23 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class FieldTester extends Activity {
 
 	boolean isUndoEnabled = false;
 	WriterUtility mWriter = new WriterUtility(this);
+	ProgressBar pb;
 
-	protected void onCreate(Bundle savedInstanceState) 
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.field_test);
+		pb = (ProgressBar) findViewById(R.id.progress_bar);
 	}
 
-	public void onClick_StartWrite(View view) throws IOException 
-	{
+	public void onClick_StartWrite(View view) throws IOException {
 		mWriter.startTest();
 		Button btnStart = (Button) findViewById(R.id.StartWrite);
 		btnStart.setEnabled(false);
@@ -36,44 +38,51 @@ public class FieldTester extends Activity {
 		btnUndo.setEnabled(false);
 	}
 
-	public void onClick_StopAndSave(View view) throws IOException 
-	{
+	public void onClick_StopAndSave(View view) throws IOException {
 		mWriter.endTestAndSave();
 		Button btnStart = (Button) findViewById(R.id.StartWrite);
 		btnStart.setEnabled(true);
 		Button btnStop = (Button) findViewById(R.id.StopAndSave);
 		btnStop.setEnabled(false);
-		
+
 		Button btnRecordStep = (Button) findViewById(R.id.RecordStep);
 		btnRecordStep.setEnabled(false);
 		Button btnUndo = (Button) findViewById(R.id.Undo);
 		btnUndo.setEnabled(false);
 	}
 
-	public void onClick_RecordStep(View view) throws IOException 
-	{
-		int n = mWriter.writeStep(new ArrayList<String>(Arrays.asList(
-				"name of device", "RSSI", "MAC address")));
-		
+	public void onClick_RecordStep(View view) throws IOException,
+			InterruptedException {
+
+		pb.setVisibility(ProgressBar.VISIBLE);
+		LinearLayout buttonsLayout = (LinearLayout) findViewById(R.id.linear_layout2);
+		buttonsLayout.setVisibility(View.INVISIBLE);
+
 		if (isUndoEnabled == false) {
 			Button btnUndo = (Button) findViewById(R.id.Undo);
 			btnUndo.setEnabled(true);
 			isUndoEnabled = true;
 		}
+
+		int n = mWriter.writeStep(new ArrayList<String>(Arrays.asList(
+				"name of device", "RSSI", "MAC address")));
+
+		pb.setVisibility(ProgressBar.INVISIBLE);
+		buttonsLayout.setVisibility(View.VISIBLE);
 		
-		//Change number on button
+		// Change number on button
 		Button btnRecordStep = (Button) findViewById(R.id.RecordStep);
-		btnRecordStep.setText("Record Step #" + (n+1)); 
+		btnRecordStep.setText("Record Step #" + (n + 1));
 	}
 
 	public void onClick_Undo(View view) {
 		int n = mWriter.undoStep();
-		if(n == 0){
+		if (n == 0) {
 			Button btnUndo = (Button) findViewById(R.id.Undo);
 			btnUndo.setEnabled(false);
 			isUndoEnabled = false;
 		}
 		Button btnRecordStep = (Button) findViewById(R.id.RecordStep);
-		btnRecordStep.setText("Record Step #" + (n+1));
+		btnRecordStep.setText("Record Step #" + (n + 1));
 	}
 }
