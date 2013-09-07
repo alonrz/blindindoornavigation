3636c8f05e06c61ba1888d8e20ca5f6c23eca1c4
DavidWebsterHCI <webstercello@gmail.com>
2013-09-07 16:49:46 -0700

    Added multiple devices, writer util, new threads

--- src/com/example/blindindoornavigation/MainActivity.java
+++ src/com/example/blindindoornavigation/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.blindindoornavigation;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.ParcelUuid;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.util.Log;
@@ -24,6 +29,7 @@ import android.content.IntentFilter;
 
 public class MainActivity extends Activity {
 	
+	Set<BluetoothDevice> beaconsInRange = new HashSet<BluetoothDevice>();
 	BluetoothAdapter adapter;
 	int REQUEST_ENABLE_BT = 1;
 	
@@ -69,35 +75,33 @@ public class MainActivity extends Activity {
         return true;
     }
 	
+  //Create a BroadcastReceiver for ACL connection success
+	final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+	    @SuppressLint("NewApi")
+		public void onReceive(Context context, Intent intent) {
+	        String action = intent.getAction();
+			
+	        //If this is true, then an ACL response was given by one of the beacons.  To get the beacon MAC that responded use intent.getExtras().get("android.bluetooth.device.extra.DEVICE").toString()
+	        if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
+	        	BluetoothDevice bd = (BluetoothDevice) intent.getExtras().get("android.bluetooth.device.extra.DEVICE");
+	        	d("ACL success for: " + bd.toString());
+	        	beaconsInRange.add(bd);	
+	        	d("adding: size:" + Integer.toString(beaconsInRange.size()));
+	        }
+	    }
+	};
+    
+    
 	public void onClick_Discover(View view)
     {   	
-    	//Create a BroadcastReceiver for ACL connection success
-    	final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-    	    public void onReceive(Context context, Intent intent) {
-    	        String action = intent.getAction();
-    			
-    	        if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
-    	        	d("ACL success for: " + intent.getExtras().get("android.bluetooth.device.extra.DEVICE").toString());
-    	        }
-    	    }
-    	};
+		CalculateLocation cl = new CalculateLocation();
+		cl.start();
     	
-    	// Register the BroadcastReceiver
-    	IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_ACL_CONNECTED);
-    	registerReceiver(mReceiver, filter);// Don't forget to unregister during onDestroy    	
-    	
-    	//Spin off threads for attempted connection to expected beacons
-    	//TODO make this list not hard coded, but gathered from an external map-based file.
-		ArrayList<String> stringList = new ArrayList<String>();
-		stringList.add("90:00:4E:FE:34:E1");//Laptop
-		stringList.add("78:A3:E4:A8:7E:48");//Phone
 		
-		for(int i=0; i<stringList.size(); i++){
-			AcceptThread at = new AcceptThread(stringList.get(i));
-			at.start();
-		}
+		//calculateLocation();
     }
     
+	
     @Override
 	protected void onStop() {
 		// TODO Auto-generated method stub
@@ -152,7 +156,6 @@ public class MainActivity extends Activity {
 	        //This try/catch block will always end in an IOexception. 
 	        try{ 
 	        	a = macTest.createInsecureRfcommSocketToServiceRecord(t);
-	        	adapter.cancelDiscovery();
 	        	adapter.listenUsingInsecureRfcommWithServiceRecord("blind", t);
 	        	a.connect();
 	        }catch(Exception e){
@@ -160,6 +163,128 @@ public class MainActivity extends Activity {
 	        }	   
 	    }
 	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////Calculate Location Thread///////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////
+
+	private class CalculateLocation extends Thread {
+
+		public CalculateLocation() {
+
+		}
+
+		//Create a BroadcastReceiver for ACL connection success
+		final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+		    @SuppressLint("NewApi")
+			public void onReceive(Context context, Intent intent) {
+		        String action = intent.getAction(); 
+				
+		        //If this is true, then an ACL response was given by one of the beacons.  To get the beacon MAC that responded use intent.getExtras().get("android.bluetooth.device.extra.DEVICE").toString()
+		        if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
+		        	BluetoothDevice bd = (BluetoothDevice) intent.getExtras().get("android.bluetooth.device.extra.DEVICE");
+		        	d("ACL success for: " + bd.toString());
+		        	beaconsInRange.add(bd);	
+		        	d("adding: size:" + Integer.toString(beaconsInRange.size()));
+		        }
+		    }
+		};
+		
+	
+		@SuppressLint("NewApi")
+		public void run() {
+			
+			// Register the BroadcastReceiver
+	    	IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_ACL_CONNECTED);
+	    	IntentFilter filterTwo = new IntentFilter(BluetoothDevice.EXTRA_RSSI);
+	    	registerReceiver(mReceiver, filter);// Don't forget to unregister during onDestroy   
+	    	registerReceiver(mReceiver, filterTwo);// Don't forget to unregister during onDestroy   
+			
+			//Spin off threads for attempted connection to expected beacons
+	    	//TODO make this list not hard coded, but gathered from an external map-based file.
+			ArrayList<String> stringList = new ArrayList<String>();
+			stringList.add("90:00:4E:FE:34:E1"); //DLaptop
+			stringList.add("78:A3:E4:A8:7E:48"); //DPhone
+			//stringList.add("3C:D0:F8:6B:16:32"); //Bphone 
+			stringList.add("20:C9:D0:85:58:5A"); //Alap
+			stringList.add("D0:23:DB:24:81:46"); //APhone 
+			stringList.add("38:0A:94:A8:F8:76"); //Aandroid 
+			
+			
+			adapter.cancelDiscovery();
+			for(int i=0; i<stringList.size(); i++){
+				AcceptThread at = new AcceptThread(stringList.get(i));  
+				at.start();
+			}
+			
+			//Wait 5 seconds for threads to all return (~5 seconds for time out, 2-4 for ACL connection return)
+			try {
+				Thread.sleep(20000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			calculateLocation();
+			
+		}
+		
+		
+		private void calculateLocation(){
+			//TODO Calculations for location.  
+			//AT this point in execution 'beaconsInRange' is a set of bluetooth 'beacons' (devices) that are 'seen' 
+			//For our test
+			
+			d(beaconsInRange.toString());
+			
+			List<String> ls = new ArrayList<String>();
+			List<String> lsSorted = new ArrayList<String>();
+			Iterator<BluetoothDevice> iter = beaconsInRange.iterator();
+			while (iter.hasNext()) {
+				ls.add(iter.next().toString());
+			}
+			
+			WriterUtility wt = new WriterUtility();
+			d("list: " + ls.toString());
+			
+
+			
+			if(ls.contains("90:00:4E:FE:34:E1")){//DLaptop
+				lsSorted.add("90:00:4E:FE:34:E1");
+			}else {lsSorted.add("null");}
+			
+			if(ls.contains("78:A3:E4:A8:7E:48")){//DPhone
+				lsSorted.add("78:A3:E4:A8:7E:48");
+			}else{lsSorted.add("null");}
+			
+			if(ls.contains("20:C9:D0:85:58:5A")){//Alap
+				lsSorted.add("20:C9:D0:85:58:5A");
+			}else{lsSorted.add("null");}
+			
+			if(ls.contains("D0:23:DB:24:81:46")){//APhone
+				lsSorted.add("D0:23:DB:24:81:46");
+			}else{lsSorted.add("null");}
+			
+			if(ls.contains("38:0A:94:A8:F8:76")){//Aandroid
+				lsSorted.add("38:0A:94:A8:F8:76"); 
+			}else{lsSorted.add("null");}
+			
+			
+			
+			try {
+				d("before pass: " + lsSorted.toString());
+				wt.writeStep(lsSorted);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+
+		}
+		
+	}
+	
+	
 }
 
 
