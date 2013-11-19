a50c29b5c2fbb8c7171b61320e1e3b4a6edde879
alon <alon.zilberman@gmail.com>
2013-11-19 15:51:00 -0800

    interface changed to add another function which will be called at each event of step

--- src/com/example/blindindoornavigation/StepsListener.java
+++ src/com/example/blindindoornavigation/StepsListener.java
@@ -5,9 +5,19 @@ package com.example.blindindoornavigation;
  */
 public interface StepsListener {
 	/**
-	 * This method will be called every time a step has been taken. 
+	 * This method will be called every time a step has been taken and give number 
+	 * of steps taken since start or last reset request. 
 	 * @param steps - the total amount of steps so far
 	 */
 	public void stepHasBeenTaken(int steps);
+	
+	/**
+	 * This method will be called every time a step has been taken 
+	 * and provide the distance traveled since start or since last reset requested.
+	 * Note the boolean value which tells if the distance is after calibration.
+	 * @param hasAccurateDistance - if false - distance is estimated. if true means calibration was performed. 
+	 * @param totalDistance - number of steps * step length = distance traveled since reset or start. 
+	 */
+	public void stepHasBeenTaken(boolean hasAccurateDistance, int totalDistance);
 	
 }
