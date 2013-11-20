248ce90c95c7466c2783746e0789aff4fd133ccc
alon <alon.zilberman@gmail.com>
2013-11-19 21:37:02 -0800

    Added new class seperate form the UI

--- src/com/example/blindindoornavigation/SensorActivity.java
+++ src/com/example/blindindoornavigation/SensorActivity.java
@@ -9,6 +9,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -23,73 +24,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class SensorActivity extends Activity implements SensorEventListener {
+public class SensorActivity extends Activity implements SensorEventListener, StepsListener {
 
-	private SensorManager mSensorManager;
-	private Sensor mOrientation, mAccelerometer, mLinearAccelerometer;
-//	private float[] mOrientation2 = new float[3];
-//	private float[] mRotation = new float[9];
-//	private float[] mRemapedRotation = new float[9];
-	private float[] mAccel = new float[3];
-	private float[] mLinearAccel = new float[3];
-	private float[] mGeoMagnetic = new float[3];
-//	private float mAzimuth;
-
-	private float offsetY, offsetX, offsetZ;
-	private boolean offset_set = false;
-	private boolean mFailed, isSensorRunning = true;
-
-	TextView txtAzimuth, txtZ_axis, txtX_axis, txtY_axis, txtVelocity, txtSteps, txtDistance;
+	StepsManager mStepsManager = StepsManager.getStepsManager();
+	
+	TextView txtAzimuth, txtZ_axis, txtX_axis, txtY_axis, txtVelocity,
+			txtSteps, txtDistance;
 	Button btnSave;
 	DecimalFormat df = new DecimalFormat();
-	WriterUtility writer;
-
-	long interval, lastEvent;
-	double lastVelocity, lastAccel;
-	
-	//vars for calculating steps
-	double maxTotal, minTotal, maxLocal, minLocal, deltaPositive = 1, deltaNegative = 1;
-	int mSteps=0;
-	static List<StepsListener> mListeners = new ArrayList<StepsListener>();
-	boolean isPositive = true;
-		
-	//Given file name is formatted to reflect the time now. 
 	String lastFileNameSaved = new Time() {{ setToNow(); }}.format2445();
-	private int mStepLength = 60; //The size of each step in cm
-	private int mDistanceWalked = 0;	
-		
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sensor_activity_layout);
-
-		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-		mOrientation = mSensorManager
-				.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-		mAccelerometer = mSensorManager
-				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		mLinearAccelerometer = mSensorManager
-				.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-
-		df.setMaximumFractionDigits(4);
-		this.onPause(); //stop the sensors. 
-		
-
-	}
-
-	protected void onResume() {
-		super.onResume();
-		
-	}
-
-	private void registerListeners() {
-		isSensorRunning = true;
-		mSensorManager.registerListener(this, mOrientation,
-				SensorManager.SENSOR_DELAY_NORMAL);
-		mSensorManager.registerListener(this, mAccelerometer,
-				SensorManager.SENSOR_DELAY_NORMAL);
-		mSensorManager.registerListener(this, mLinearAccelerometer,
-				SensorManager.SENSOR_DELAY_NORMAL);
 
 		txtAzimuth = (TextView) findViewById(R.id.txtAzimuth);
 		txtZ_axis = (TextView) findViewById(R.id.txtZ_axis);
@@ -99,15 +47,19 @@ public class SensorActivity extends Activity implements SensorEventListener {
 		btnSave = (Button) findViewById(R.id.btnSaveToFile);
 		txtSteps = (TextView) findViewById(R.id.txtSteps);
 		txtDistance = (TextView) findViewById(R.id.txtDistance);
+
+		df.setMaximumFractionDigits(4);
 		
-		writer = new WriterUtility(this);
-		writer.startTest();
+		this.onPause(); // stop the sensors.
+	}
+
+	protected void onResume() {
+		super.onResume();
 	}
 
 	protected void onPause() {
 		super.onPause();
-		isSensorRunning = false;
-		mSensorManager.unregisterListener(this);
+		//isSensorRunning = false;
 	}
 
 	void onSuccess() {
@@ -120,227 +72,119 @@ public class SensorActivity extends Activity implements SensorEventListener {
 
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		// TODO Auto-generated method stub
-
+		// Nothing happens here
 	}
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 
-		switch (event.sensor.getType()) {
-		case Sensor.TYPE_ACCELEROMETER:
-			for (int i = 0; i < 3; i++)
-				mAccel[i] = event.values[i];
-			break;
-		case Sensor.TYPE_MAGNETIC_FIELD:
-			for (int i = 0; i < 3; i++)
-				mGeoMagnetic[i] = event.values[i];
-			break;
-		case Sensor.TYPE_LINEAR_ACCELERATION:
-			for (int i = 0; i < 3; i++)
-				mLinearAccel[i] = event.values[i];
-			break;
-		default:
-			return;
-		}
-
-		// Register offsets:
-		if (offset_set == false) {
-			offsetX = mLinearAccel[0];
-			offsetY = mLinearAccel[1];
-			offsetZ = mLinearAccel[2];
-		}
-		offset_set = true;
-
-		/*
-		 * values[0]: Acceleration minus Gx on the x-axis values[1]:
-		 * Acceleration minus Gy on the y-axis values[2]: Acceleration minus Gz
-		 * on the z-axis
-		 */
-		txtX_axis.setText("X axis: " + df.format(mLinearAccel[0])
-				+ "\n  (with offset = "
-				+ (df.format(mLinearAccel[0] - offsetX)) + ")");
-		txtY_axis.setText("Y axis: " + df.format(mLinearAccel[1])
-				+ "\n  (with offset = "
-				+ (df.format(mLinearAccel[1] - offsetY)) + ")");
-		txtZ_axis.setText("Z axis: " + df.format(mLinearAccel[2])
-				+ "\n  (with offset = "
-				+ (df.format(mLinearAccel[2] - offsetZ)) + ")");
-
-		long now = System.currentTimeMillis();
-		interval = Math.abs(lastEvent - now);
-		lastEvent = now;
-
-		lastVelocity = lastVelocity + lastAccel * (interval / 1000000.);
-		/*
-		 * V = V_0 + AT where A is the previous accel applied on the interval
-		 * passed To calculate the velocity traveled. V_0 is the initial
-		 * velocity at each time interval passed so we keep updating it.
-		 */
-		//lastAccel = mLinearAccel[1] - offsetY; // Y- axis
-		lastAccel = mLinearAccel[1] - offsetY + mLinearAccel[0]-offsetX +
-				mLinearAccel[2]-offsetZ; // ALL axis
-
-		txtVelocity.setText("velocity: " + df.format(lastVelocity) + " m/s");
-		Log.i("Accel", Double.toString(lastAccel));
-		writer.writeValue(Double.toString(lastAccel));
-		CalculateSteps(lastAccel);
+	
 	}
-	
-	/**
-	 * Register your instance to get event alert for steps taken
-	 * @param listener an instance of StepsListener
-	 */
-	public static void RegisterStepsListener(StepsListener listener)
-	{
-		mListeners.add(listener);
-	}
-	
-	void CalculateSteps(double lastAccel)
-	{
-		//if acceleration is in delta (buffer) it should be ignored. 
-		if(lastAccel > 0 && lastAccel < deltaPositive) return;
-		if(lastAccel < 0 && lastAccel > deltaNegative) return;
-		
-		//total max and min
-		if(maxTotal<lastAccel)
-		{
-			maxTotal = lastAccel;
-			deltaPositive = maxTotal/3;
-		}
-		if(minTotal>lastAccel) 
-		{
-			minTotal = lastAccel;
-			deltaNegative = minTotal/3;
-		}
-		
-		//Check if transitioned from positive to negative or other way
-		if(isPositive == true)
-		{
-			if(lastAccel < deltaNegative) isPositive = false;
-		}
-		else
-		{
-			if(lastAccel > deltaPositive) 
-			{
-				isPositive = true;
-				stepDetected(++mSteps);
-			}
-		}
-	}
-	
-	/**
-	 * This method will call all listeners and alert them of the number of steps
-	 * @param steps - takes an integer off total number of steps.
-	 */
-	void stepDetected(int steps)
-	{
-		//Call all listeners
-		for(StepsListener sl : mListeners)
-			sl.stepHasBeenTaken(steps);
-		
-		//Write to UI for debug
-		txtSteps.setText("Steps: " + steps);
-		mDistanceWalked = mSteps * mStepLength;
-		txtDistance.setText("Distance: " + mDistanceWalked);
-	}
-	
-	
+
+
 	/************************************
-	 **     onClick Events below       **
+	 ** onClick Events below 
 	 ************************************/
-	
-	public void onClick_RestartSensors(View view)
+
+	public void onClick_Start(View view) throws Exception
 	{
-		registerListeners();
-		mSteps = 0;
-		mDistanceWalked = 0;
+		mStepsManager.registerListener(this);
 	}
 	
-	public void onClick_Save(View view)
-	{
-		//Pause activity
-		this.onPause();
-		
-		//Ask use for file name
-		AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
-		alertBuilder.setTitle("Save to File");
-		alertBuilder.setMessage("Enter File Name (no extention): ");
-		
-		final EditText input = new EditText(this);
-		input.setText(lastFileNameSaved); //insert last text
-		input.selectAll();
-		alertBuilder.setView(input);
-		
-		alertBuilder.setPositiveButton("OK", new OnClickListener() {
-			
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				lastFileNameSaved = input.getText().toString();
-				
-				//Save to file
-				try {
-					writer.endTestAndSave(lastFileNameSaved + " " + new Time() {{ setToNow(); }}.format2445());
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		});
-		alertBuilder.setNegativeButton("Cancel", new OnClickListener() {
-			
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		
-		AlertDialog alert = alertBuilder.create();
-		alert.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
-		alert.show();
-	}
-	
-	public void onClick_Cancel(View view)
-	{
+	public void onClick_Cancel(View view) {
+		mStepsManager.unregisterListener();
 		finish();
 	}
 	
-	public void onClick_StartCalibrate(View view)
-	{	
-		//Ask use for file name
+	public void onClick_RestartSensors(View view) {
+		mStepsManager.reset();
+	}
+
+	public void onClick_Save(View view) {
+		// Pause activity
+		this.onPause();
+
+		// Ask use for file name
 		AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
-		alertBuilder.setTitle("Calibrate Steps");
-		alertBuilder.setMessage("Press 'OK' and walk normally for 9m (~30f). Press Stop once you're done.");
-		
+		alertBuilder.setTitle("Save to File");
+		alertBuilder.setMessage("Enter File Name (no extention): ");
+
+		final EditText input = new EditText(this);
+		input.setText(lastFileNameSaved); // insert last text
+		input.selectAll();
+		alertBuilder.setView(input);
+
 		alertBuilder.setPositiveButton("OK", new OnClickListener() {
-			
+
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
+				lastFileNameSaved = input.getText().toString();
+
+				// Save to file
+				mStepsManager.writeToFile(lastFileNameSaved);
 				
-				//Start calibration - reset fields
-				mSteps = 0;
-				mDistanceWalked = 0;
 			}
 		});
 		alertBuilder.setNegativeButton("Cancel", new OnClickListener() {
-			
+
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				// Do thing. Cancel was pressed. 
+				// TODO Auto-generated method stub
+
 			}
 		});
-		
+
+		AlertDialog alert = alertBuilder.create();
+		alert.getWindow().setSoftInputMode(
+				WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+		alert.show();
+	}
+
+	public void onClick_StartCalibrate(View view) {
+		// Ask use for file name
+		AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+		alertBuilder.setTitle("Calibrate Steps");
+		alertBuilder
+				.setMessage("Press 'OK' and walk normally for 9m (~30f). Press Stop once you're done.");
+
+		alertBuilder.setPositiveButton("OK", new OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+
+				// Start calibration - reset fields
+				mStepsManager.reset();
+			}
+		});
+		alertBuilder.setNegativeButton("Cancel", new OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// Do thing. Cancel was pressed.
+			}
+		});
+
 		AlertDialog alert = alertBuilder.create();
 		alert.show();
 	}
-	
-	public void onClick_StopCalibrate(View view)
+
+	public void onClick_StopCalibrate(View view) 
+	{	
+		txtDistance.setText(String.valueOf(mStepsManager.stopCalibration()));
+	}
+
+	@Override
+	public void stepHasBeenTaken(int steps) 
 	{
-		if(mSteps != 0)
-			mStepLength = 900/mSteps;
-		txtDistance.setText(String.valueOf(mStepLength));
+		txtSteps.setText("Steps: " + steps);
+	}
+
+	@Override
+	public void stepHasBeenTaken(boolean hasAccurateDistance, int totalDistance) {
+		if(hasAccurateDistance == false)
+			return;
+		
+		txtDistance.setText("Distance: " + totalDistance);
+		
 	}
 }
-
-
---
+++ src/com/example/blindindoornavigation/StepsManager.java
@@ -0,0 +1,277 @@
+package com.example.blindindoornavigation;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.text.format.Time;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class StepsManager implements SensorEventListener {
+
+	/************* Singleton Section ******************/
+	private static StepsManager mStepsManager = null;
+
+	private StepsManager() {
+	}
+
+	/**
+	 * Getter for Singleton class StepsManager
+	 * 
+	 * @return a singleton instance of StepsManager
+	 */
+	public static StepsManager getStepsManager() {
+		if (mStepsManager == null)
+			mStepsManager = new StepsManager();
+		return mStepsManager;
+	}
+
+	/************ end Singleton Section ******************/
+
+	private Activity mActivity;
+	private SensorManager mSensorManager;
+	private Sensor mOrientation, mAccelerometer, mLinearAccelerometer;
+	private float[] mAccel = new float[3];
+	private float[] mLinearAccel = new float[3];
+	private float[] mGeoMagnetic = new float[3];
+
+	private float offsetY, offsetX, offsetZ;
+	private boolean offset_set = false;
+	private boolean mFailed, isSensorRunning = true;
+
+	WriterUtility writer;
+
+	long interval, lastEvent;
+	double lastVelocity, lastAccel;
+
+	// vars for calculating steps
+	double maxTotal = 0.5, minTotal = -0.5, maxLocal, minLocal,
+			deltaPositive = .5, deltaNegative = .5;
+	int mSteps = 0;
+	boolean isPositive = true;
+
+	// Given file name is formatted to reflect the time now.
+	private int mStepLength; // The size of each step in cm
+	private int mDistanceWalked = 0;
+	private SharedPreferences mSharedPreferences;
+	private boolean mHasStepLength = false; // means callibration was performed.
+
+	/**
+	 * Register your Activity class to get event calls.
+	 * 
+	 * @param activity
+	 *            - Must be a subclass of Activity as well as implement the
+	 *            interface
+	 * @throws Exception
+	 *             If activity is not a subclass of Activity
+	 */
+	public void registerListener(StepsListener activity) throws Exception {
+		if (!(activity instanceof Activity))
+			throw new Exception(
+					"Not a subclass of Activity. Must be for use of SharedPreferences etc...");
+		mActivity = (Activity) activity;
+
+		// Get step length is available
+		mSharedPreferences = mActivity.getPreferences(mActivity.MODE_PRIVATE);
+		mStepLength = mSharedPreferences.getInt("mStepLength", 60);
+
+		mSensorManager = (SensorManager) mActivity
+				.getSystemService(mActivity.SENSOR_SERVICE);
+		mOrientation = mSensorManager
+				.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+		mAccelerometer = mSensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mLinearAccelerometer = mSensorManager
+				.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+		registerListeners();
+	}
+
+	/**
+	 * Unregister your instance from event alerts
+	 * 
+	 * @param listener
+	 *            an instance of StepsListener
+	 */
+	public void unregisterListener() {
+		mSensorManager.unregisterListener(this);
+		mActivity = null;
+	}
+
+	/**
+	 * Register to all sensors are here. Also allocating UI objects.
+	 */
+	private void registerListeners() {
+		isSensorRunning = true;
+		mSensorManager.registerListener(this, mOrientation,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		mSensorManager.registerListener(this, mAccelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		mSensorManager.registerListener(this, mLinearAccelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+
+		writer = new WriterUtility(mActivity);
+		writer.startTest();
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		switch (event.sensor.getType()) {
+		case Sensor.TYPE_ACCELEROMETER:
+			for (int i = 0; i < 3; i++)
+				mAccel[i] = event.values[i];
+			break;
+		case Sensor.TYPE_MAGNETIC_FIELD:
+			for (int i = 0; i < 3; i++)
+				mGeoMagnetic[i] = event.values[i];
+			break;
+		case Sensor.TYPE_LINEAR_ACCELERATION:
+			for (int i = 0; i < 3; i++)
+				mLinearAccel[i] = event.values[i];
+			break;
+		default:
+			return;
+		}
+
+		// Register offsets:
+		if (offset_set == false) {
+			offsetX = mLinearAccel[0];
+			offsetY = mLinearAccel[1];
+			offsetZ = mLinearAccel[2];
+		}
+		offset_set = true;
+
+		/*
+		 * values[0]: Acceleration minus Gx on the x-axis values[1]:
+		 * Acceleration minus Gy on the y-axis values[2]: Acceleration minus Gz
+		 * on the z-axis
+		 */
+
+		// txtX_axis.setText("X axis: " + df.format(mLinearAccel[0])
+		// + "\n  (with offset = "
+		// + (df.format(mLinearAccel[0] - offsetX)) + ")");
+		// txtY_axis.setText("Y axis: " + df.format(mLinearAccel[1])
+		// + "\n  (with offset = "
+		// + (df.format(mLinearAccel[1] - offsetY)) + ")");
+		// txtZ_axis.setText("Z axis: " + df.format(mLinearAccel[2])
+		// + "\n  (with offset = "
+		// + (df.format(mLinearAccel[2] - offsetZ)) + ")");
+
+		long now = System.currentTimeMillis();
+		interval = Math.abs(lastEvent - now);
+		lastEvent = now;
+
+		lastVelocity = lastVelocity + lastAccel * (interval / 1000000.);
+		/*
+		 * V = V_0 + AT where A is the previous accel applied on the interval
+		 * passed To calculate the velocity traveled. V_0 is the initial
+		 * velocity at each time interval passed so we keep updating it.
+		 */
+		// lastAccel = mLinearAccel[1] - offsetY; // Y- axis
+		lastAccel = mLinearAccel[1] - offsetY + mLinearAccel[0] - offsetX
+				+ mLinearAccel[2] - offsetZ; // ALL axis
+
+		Log.i("Accel", Double.toString(lastAccel));
+		writer.writeValue(Double.toString(lastAccel));
+		CalculateSteps(lastAccel);
+
+	}
+
+	void CalculateSteps(double lastAccel) {
+		// if acceleration is in delta (buffer) it should be ignored.
+		if (lastAccel > 0 && lastAccel < deltaPositive)
+			return;
+		if (lastAccel < 0 && lastAccel > deltaNegative)
+			return;
+
+		// total max and min
+		if (maxTotal < lastAccel) {
+			maxTotal = lastAccel;
+			deltaPositive = maxTotal / 2;
+		}
+		if (minTotal > lastAccel) {
+			minTotal = lastAccel;
+			deltaNegative = minTotal / 2;
+		}
+
+		// Check if transitioned from positive to negative or other way
+		if (isPositive == true) {
+			if (lastAccel < deltaNegative)
+				isPositive = false;
+		} else {
+			if (lastAccel > deltaPositive) {
+				isPositive = true;
+				stepDetected(++mSteps);
+			}
+		}
+	}
+
+	/**
+	 * This method will call all listeners and alert them of the number of steps
+	 * 
+	 * @param steps
+	 *            - takes an integer off total number of steps.
+	 */
+	void stepDetected(int steps) {
+		((StepsListener) mActivity).stepHasBeenTaken(steps);
+		((StepsListener) mActivity).stepHasBeenTaken(mHasStepLength, steps
+				* mStepLength);
+	}
+
+	/**
+	 * This method will reset both the steps counter and distance measured.
+	 * Events will still be fired until unregister is complete.
+	 */
+	public void reset() {
+		mSteps = 0;
+		mDistanceWalked = 0;
+	}
+
+	/**
+	 * Start calibration for 9 m
+	 */
+	public void startCalibration() {
+		mSteps = 0;
+	}
+
+	/**
+	 * Stop calibration test after 9m.
+	 * 
+	 * @return length of a single step
+	 */
+	public int stopCalibration() {
+		if (mSteps != 0) {
+			mStepLength = 900 / mSteps;
+			mSharedPreferences.edit().putInt("mStepLength", mStepLength);
+			mHasStepLength = true;
+		}
+		return mStepLength;
+	}
+
+	public void writeToFile(String lastFileNameSaved) {
+		try {
+			writer.endTestAndSave(lastFileNameSaved + " " + new Time() {
+				{
+					setToNow();
+				}
+			}.format2445());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
