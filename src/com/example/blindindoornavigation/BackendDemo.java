3d4317bdcdfa9955579d975c9d63b3da00fe9090
DavidWebsterHCI <webstercello@gmail.com>
2014-01-04 21:05:33 -0800

    t

--- src/com/example/blindindoornavigation/BackendDemo.java
+++ src/com/example/blindindoornavigation/BackendDemo.java
@@ -1,30 +1,50 @@
 package com.example.blindindoornavigation;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
+import android.widget.Toast;
+/**
+ * Note:  Backend demo was a quick-and-dirty way to demonstrate the backend systems are functional and returning correct data. 
+ * 		  Therefore any duct tape/questionable programming was intentionally overlooked. Use at your own risk.
+ *
+ */
 public class BackendDemo extends Activity implements LocationListener, StepsListener {
 
 
+	/////ORIENTATION
+	private static StringBuilder sb;
+	private static int n = 0; // n = the number of records (lines) added.
+
+	private SensorManager mSensorManager;
+	private Sensor sensor;
+	private float mAzimuth;
+	private Context mContext;
+	//////
+
 	ProgressBar pb;
-	
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.backend_demo);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		pb = (ProgressBar) findViewById(R.id.progress_bar);
-		
+
 		DiscoverLocation d = new DiscoverLocation(this);
 		d.InitBluetoothLocationServices();
 		d.registerLocationListener(this);
 		d.scanForDevices();
-		
-		
+
+
 		StepsManager s = StepsManager.getStepsManager();
 		try {
 			s.registerListener(this);
@@ -32,24 +52,27 @@ public class BackendDemo extends Activity implements LocationListener, StepsList
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
+		startOrientation();
 	}
 
 	@Override
 	public void newLocationHasBeenCalculated(final String LocationCalculated) {
-		d("Newlocationhasbeencalculated in callback: " + LocationCalculated);
 
-		final Integer loc = Integer.parseInt(LocationCalculated);
-		
-		
+		d("Newlocationhasbeencalculated in callback: " + LocationCalculated);
+		final Integer loc;
+		if(LocationCalculated != null)
+			loc = Integer.parseInt(LocationCalculated);
+		else
+			loc = -1;
+
 		runOnUiThread(new Runnable() {
 			@Override
 			public void run() {	
-				
+
 				ImageView i = (ImageView) findViewById(R.id.imageView1);
-				
-				
+
+
 				switch(loc){
 				case 0:
 					i.setImageResource(R.drawable.loc1);
@@ -73,17 +96,58 @@ public class BackendDemo extends Activity implements LocationListener, StepsList
 					i.setImageResource(R.drawable.loc7);
 					break;
 				}
-			
-				
-				
-				
-				
-				
-				TextView ts = (TextView) findViewById(R.id.editText1);
+
+				TextView ts = (TextView) findViewById(R.id.txtBluetoothLocation);
 				ts.setText(LocationCalculated);
 			}
 		});
 	}
+
+
+	@Deprecated
+	public void startOrientation() {
+		sb = new StringBuilder();
+
+		// Sensor init
+		mContext = this;
+		mSensorManager = (SensorManager) mContext
+				.getSystemService(Context.SENSOR_SERVICE);
+
+		sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+
+		if (sensor != null) {
+			mSensorManager.registerListener(mySensorEventListener, sensor,
+					SensorManager.SENSOR_DELAY_UI);
+			d("Registerered for ORIENTATION Sensor");
+
+		} else {
+			d("ORIENTATION Sensor not found");
+			Toast.makeText(mContext, "ORIENTATION Sensor not found",
+					Toast.LENGTH_LONG).show();
+		}
+	}
+
+	private SensorEventListener mySensorEventListener = new SensorEventListener() {
+
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onSensorChanged(final SensorEvent event) {
+			runOnUiThread(new Runnable() {
+				@Override
+				public void run() {	
+
+					mAzimuth = (int) event.values[0];
+					TextView ts = (TextView) findViewById(R.id.txtOrientation); //INTENTIONAL
+					ts.setText(Float.toString(mAzimuth));
+				}
+			});
+		}
+	};
 
 	//Debug log function
 	public static void d(String s){  
@@ -96,11 +160,11 @@ public class BackendDemo extends Activity implements LocationListener, StepsList
 		runOnUiThread(new Runnable() {
 			@Override
 			public void run() {	
-				TextView ts = (TextView) findViewById(R.id.editText2);
+				TextView ts = (TextView) findViewById(R.id.txtStepsTaken);
 				ts.setText(Integer.toString(steps));
 			}
 		});
-		
+
 	}
 
 	@Override
